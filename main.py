576f064165f81a20c631d190ef4ee9f7014a3b12
Аудитория 7 <aud7_st@step.local>
2024-03-17 16:04:54 +0200

    create human

--- main.py
+++ main.py
@@ -1,16 +1,74 @@
-# This is a sample Python script.
+from random import*
+class Student:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, name):
+        self.name = name
+
+        #радість
+        self.gladness = 50
+
+        #прогрес в розвитку
+        self.progress = 0
+
+        #чии живі
+        self.alive = True
+
+    def to_study(self):
+        print("Time to STUDY!!! ;( ")
+        self.progress += 0.12
+        self.gladness -= 3
+
+    def to_sleep(self):
+        print("I will sleep")
+        self.gladness += 3
+
+    def to_chill(self):
+        print("Rest time")
+        self.progress += 5
+        self.gladness -= 0.1
+
+    def is_alive(self):
+
+       if self.progress < -0.5:
+           print("Cast out")
+           self.alive = False
+
+       elif self.gladness <=0:
+           print("Depression")
+           self.alive = False
+
+       elif self.progress > 5:
+           print("passed externally...")
+           self.alive = False
+
+    def end_of_day(self):
+        print(f"Gladness = {self.gladness}")
+        print(f"Progress = {self.progress}")
+        print()
+
+    def live(self,day):
+        print(f"Day - {day} of {self.name}")
+
+        live_cube = randint(1,3)
+        if live_cube == 1:
+            self.to_study()
+
+        elif live_cube == 2:
+            self.to_chill()
+
+        elif live_cube == 3:
+            self.to_sleep()
+
+        self.end_of_day()
+
+        self.is_alive()
+
+Diana = Student("Diana")
+
+for day in range(365):
+    if Diana.alive == False:
+        break
+
+    Diana.live(day)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
